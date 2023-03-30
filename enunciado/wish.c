e86473b4870bd11da19e5f2c4c2476b6687aac17
Giloc <giloc.delgado@udea.edu.co>
2023-03-30 11:43:05 -0500

    verificación de sintaxis con redirección

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -13,15 +13,44 @@ char *mypath[] = { "bli","bla","/bin/",""};
 int size = 4;
 
 int check_arguments(char *arguments){
+	char *arguments_copy = strdup(arguments);
 	int count = 0;
 	char *argument;
-	while((argument = strtok_r(arguments, " ", &arguments))){
-		count++;
+	while((argument = strtok_r(arguments_copy, " ", &arguments_copy))){
+		if(strcmp(argument, ">") == 0){
+			break;
+		}
+		count++;		
 	}
 	if(count <= 1){
 		return 1;
 	}
 	return 0;
+}
+
+int check_redirection(char *arguments){
+	int count = 0;
+	char *argument;
+	while((argument = strtok_r(arguments, " ", &arguments))){
+		if(strcmp(argument, ">") == 0){
+			count++;
+		}		
+	}
+	if(count == 1){
+		return 1;
+		
+	}
+	return 0;
+}
+
+int check_sintaxis(char *command){
+	if(check_arguments(command) == 0){
+		return 0;
+	}
+	else if(check_redirection(command) == 0){
+		return 0;
+	}
+	return 1;
 }
 
 int main(int argc, char *argv[]){
@@ -57,7 +86,7 @@ int main(int argc, char *argv[]){
 				strncat(specificpath, command_string, strlen(command_string));
 				fd = access(specificpath, X_OK);
 			}
-			if(check_arguments(s) == 0){
+			if(check_sintaxis(s) == 0){
 				execute_error();
 			}
 			else if(fd==0){
