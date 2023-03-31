39328dd34f8f06569660c3eebdc939156749f43a
Giloc <giloc.delgado@udea.edu.co>
2023-03-31 14:46:58 -0500

    ejercicios giloc funcionando

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <libgen.h>
 #include <sys/wait.h>
 #include "wish_utils.h"
 #include "wish_utils.c"
@@ -11,6 +12,16 @@
 
 char *mypath[] = { "bli","bla","/bin/",""};
 int size = 4;
+int redirection = 0;
+int concurrency = 0;
+int buffer_size = 1024;
+
+int check_ampersand(char *line){
+	if(strcmp(line, "&") == 0){
+		return 0;
+	}
+	return 1;
+}
 
 int check_arguments(char *arguments){
 	char *arguments_copy = strdup(arguments);
@@ -29,28 +40,78 @@ int check_arguments(char *arguments){
 }
 
 int check_redirection(char *arguments){
+	redirection = 0;
+	char *arguments_copy = strdup(arguments);
 	int count = 0;
 	char *argument;
-	while((argument = strtok_r(arguments, " ", &arguments))){
+	while((argument = strtok_r(arguments_copy, " ", &arguments_copy))){
 		if(strcmp(argument, ">") == 0){
 			count++;
+			redirection = 1;
+			
 		}		
 	}
-	if(count == 1){
+	if(count <= 1){
 		return 1;
-		
 	}
+	redirection = 0;
+	
 	return 0;
+}
+
+char** separate_commands(char *line){
+	int commands_count = 0;
+	char *command;
+	char **commands = malloc(sizeof(char*) * MAX_SIZE);
+	while((command = strtok_r(line, "&", &line))){
+		commands[commands_count] = malloc(strlen(command) + 1);
+		strcpy(commands[commands_count], command);
+		commands_count++;
+	}
+	//commands[commands_count] = malloc(strlen(line) + 1);
+	//strcpy(commands[commands_count], line);
+	commands[commands_count] = "";
+	if(commands_count > 1){
+		concurrency = 1;
+	}
+	else{
+		concurrency = 0;
+	}
+	return commands;
 }
 
 int check_sintaxis(char *command){
 	if(check_arguments(command) == 0){
+		printf("problema con argumentos %s\n", command);
 		return 0;
 	}
 	else if(check_redirection(command) == 0){
+		printf("problema con redireccion %s\n", command);
 		return 0;
 	}
 	return 1;
+}
+
+void execute_redirection(char *command, char* filename){
+	FILE *fp;
+	char buffer[buffer_size];
+	
+	fp = popen(command, "r");
+	if(fp == NULL){
+		printf("feo\n");
+		exit(1);
+	}
+	char *dir = strtok_r(filename, " ", &filename);
+	FILE *out = fopen(dir, "w");
+	if(out == NULL){
+		printf("error abriendo archivo\n");
+		exit(1);
+	}
+	while(fgets(buffer, buffer_size, fp) != NULL){
+		fprintf(out, "%s", buffer);
+	}
+	pclose(fp);
+	fclose(out);
 }
 
 int main(int argc, char *argv[]){
@@ -59,58 +120,115 @@ int main(int argc, char *argv[]){
 	char *command_string;
 	char *s;
 	int fd;
-
-	do{
-     		printf("whish> ");
-     		fgets(str, MAX_SIZE, stdin);
-		s = str;
-                while(*s != '\n') {
-                	++s;
-                }
-                *s = '\0';
-		s = str;
-                command_string = strtok_r(s, " ", &s);
-		if(strcmp(command_string, "exit") == 0){
-			execute_exit(0);
-		}else if(strcmp(command_string, "cd") == 0){
-			execute_cd(s);
-		}else if(strcmp(command_string, "path") == 0){
-			execute_path(mypath, s, size);
-			size = 2;
-		}else{
-			fd = -1;
-			char **mp = mypath;
-			char specificpath[MAX_SIZE];
-			while( (strcmp(*mp, "") != 0)  && fd != 0){
-				strcpy(specificpath, *mp++);
-				strncat(specificpath, command_string, strlen(command_string));
-				fd = access(specificpath, X_OK);
-			}
-			if(check_sintaxis(s) == 0){
-				execute_error();
-			}
-			else if(fd==0){
-				
-				int subprocess = fork();
-				if(subprocess < 0){
-					printf("Error launching the subprocess");
-				}else if(subprocess == 0){
-					char *myargs[3];
-	  				myargs[0] = strdup(specificpath);
-	  				if(strcmp(s, "") == 0){
-	  					myargs[1] = NULL;
-	  				}
-	  				else{
-	  					myargs[1] = strdup(s);
-	  				}
-	  				myargs[2] = NULL;
-	  				execvp(myargs[0], myargs);
+	
+	if(argc == 1){
+		do{
+     			printf("whish> ");
+     			fgets(str, MAX_SIZE, stdin);
+			s = str;
+                	while(*s != '\n') {
+                		++s;
+                	}
+                	*s = '\0';
+			s = str;
+			if(check_ampersand(s) == 1){
+				char **commands = separate_commands(s);
+				char *fcommand = strdup(commands[0]);
+                		command_string = strtok_r(fcommand, " ", &fcommand);
+				if(strcmp(command_string, "exit") == 0){
+					execute_exit(0);
+				}else if(strcmp(command_string, "cd") == 0){
+					execute_cd(fcommand);
+				}else if(strcmp(command_string, "path") == 0){
+					execute_path(mypath, fcommand, size);
+					size = 2;
 				}else{
-					wait(NULL);
+					int i = 0;
+					int childrens = 0;
+					while(strcmp(commands[i], "") != 0){
+						char *command = commands[i];
+                				command_string = strtok_r(command, " ", &command);
+						fd = -1;
+						char **mp = mypath;
+						char specificpath[MAX_SIZE];
+						while( (strcmp(*mp, "") != 0)  && fd != 0){
+							strcpy(specificpath, *mp++);
+							strncat(specificpath, command_string, strlen(command_string));
+							fd = access(specificpath, X_OK);
+						}
+						if(check_sintaxis(command) == 0){
+							execute_error(153);
+						}
+						else if(fd==0){
+						
+							if(redirection == 0){
+								int subprocess = fork();
+								if(subprocess < 0){
+									printf("Error launching the subprocess");
+								}else if(subprocess == 0){
+									char *myargs[3];
+	  								myargs[0] = strdup(specificpath);
+	  								if(strcmp(command, "") == 0){
+	  									myargs[1] = NULL;
+	  								}
+	  								else{
+	  									myargs[1] = strdup(command);
+	  								}
+	  								myargs[2] = NULL;
+	  								execvp(myargs[0], myargs);
+								}else{
+									childrens++;
+								}
+							}
+							else if(redirection == 1){
+								int subprocess = fork();
+								if(subprocess < 0){
+									printf("Error launching the subprocess");
+								}
+								else if(subprocess == 0){
+									char *arguments;
+									strcat(specificpath, " ");
+									if(command[0] == 62){
+										arguments = strtok_r(command, ">", &command);
+										execute_redirection(specificpath, arguments);
+									}
+									else{
+										arguments = strtok_r(command, ">", &command);
+										strcat(specificpath, arguments);
+										execute_redirection(specificpath, command);
+									}
+									exit(0);
+								}
+								else{
+									childrens++;
+								}
+							}
+							
+						}else{
+							execute_error(201);
+						}
+						i++;
+					}
+					int status;
+					while(childrens > 0){
+						pid_t pid = waitpid(-1, &status, WNOHANG);
+						if(pid > 0){
+							childrens--;
+						}
+					}
+					
 				}
-			}else{
-				execute_error();
 			}
-		}
-   	}while(1);
+			else{
+				execute_error(223);
+			}
+   		}while(1);
+	}
+	else if(argc == 2){
+		//do something
+	}
+	else{
+		execute_error(220);
+	}
+	
 }
--- enunciado/wish_utils.c
+++ enunciado/wish_utils.c
@@ -5,8 +5,8 @@
 #include "wish_utils.h"
 #define MAX_SIZE 100
 
-void execute_error(){
-	printf("An error has ocurred\n");
+void execute_error(int line){
+	printf("An error has ocurred in line %i\n", line);
 }
 
 void execute_exit(int value){
@@ -16,35 +16,51 @@ void execute_exit(int value){
 void execute_cd(char *newpath){
 	char *path = strtok_r(newpath, " ", &newpath);
 	if(path == NULL){
-		execute_error();
+		execute_error(19);
 	}
 	else{
 		if(chdir(path) == -1){
-			execute_error();
+			execute_error(23);
 		};
 	}
 }
 
-void execute_path(char** path, char *newpaths, int size){
+char* concatenate_path(char* original){
 	char *relative = "./";
+	char *thepath = strdup(original);
+	char *lp = "/";
+	char *fp = malloc(sizeof(char*) * MAX_SIZE);
+	strcpy(fp, relative);
+	strncat(fp, thepath, strlen(thepath));
+	strcat(fp, lp);
+	free(thepath);
+	return fp;
+}
+
+void execute_path(char** path, char *newpaths, int size){
+	
 	for(int i = 0; i < size; i++){
 		path[i] = NULL;	
 	}
-	if(strncmp(newpaths, ".", 1) != 0 && strncmp(newpaths, "/", 1) != 0){
-		char *thepath = strdup(newpaths);
-		char *lp = "/";
-		char fp[MAX_SIZE];
-		strcpy(fp, relative);
-		strncat(fp, thepath, strlen(thepath));
-		strcat(fp, lp);
-		path[0] = malloc(strlen(fp) + 1);
-		strcpy(path[0], fp);
-		free(thepath);
-	}
-	else{
-		path[0] = strdup(newpaths);
-	}
-	path[1] = "";
+	char* newpath;
+	int index = 0;
+	while((newpath = strtok_r(newpaths, " ", &newpaths))){
+		if(strncmp(newpath, ".", 1) != 0 && strncmp(newpath, "/", 1) != 0){
+			char *fp = concatenate_path(newpath);
+			path[index] = malloc(strlen(fp) + 1);
+			strcpy(path[index], fp);
+			free(fp);
+		}
+		else{
+			char *actualpath = strdup(newpath);
+			strcat(actualpath, "/");
+			path[index] = actualpath;
+		}
+		index++;
+	} 
+	path[index] = "";
+	
+	
 }
 
 
